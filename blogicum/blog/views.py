65b31978ac9052832f248435627acd7522a56866
Алексей Попов <vechanka@yandex.ru>
2023-12-08 19:58:15 +0300

    08.12.23 ver_5.1

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Count
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.urls import reverse, reverse_lazy
